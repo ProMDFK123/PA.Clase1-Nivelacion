32570967aea0a0eb5a854baaa326360ea97bfa5c
Gabriel Lopez <gabriel.lopez@alumnos.ucn.cl>
2023-04-24 18:25:01 -0400

    Initial commit

---
+++ src/Promedio.java
@@ -0,0 +1,24 @@
+public class Promedio {
+    /*
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Ingrese la nota numero 1: ");
+        double notaUno = in.nextDouble();
+        
+        System.out.println("Ingrese la nota 2: ");
+        double notaDos = in.nextDouble();
+        
+        System.out.println("Ingrese la nota 3: ");
+        double notaTres = in.nextDOuble();
+        
+        double notaFinal = (notaUno+notaDos+notaTres)/3;
+        if (notaFinal>=3.95){
+            System.out.println("Reprobaste");
+        }
+        else{
+            System.out.println("Aprobaste");
+        }
+    }
+     */
+}
+
